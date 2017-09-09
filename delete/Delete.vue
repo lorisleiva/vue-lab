44d26e22fdfd8275576e8a778dea4cee1faf0573
Loris LEIVA <loris.leiva@gmail.com>
2017-09-09 12:13:44 +0200

    :tada: Initial commit

---
+++ delete/Delete.vue
@@ -0,0 +1,122 @@
+
+<template>
+
+    <!-- Positioning parent. -->
+    <div class="d-flex justify-content-end align-items-center">
+
+        <!-- Message. -->
+        <transition name="fade">
+            <span 
+                v-if="showMessage"
+                v-text="message"
+                class="mr-1"
+                :class="messageClass"
+            ></span>
+        </transition>
+
+        <!-- Close icon. -->
+        <i 
+            v-show="status !== 'success'"
+            class="fa fa-close" 
+            :class="{'fa-spin': status === 'loading', red: status !== 'idle'}"
+            @click="click"
+        ></i>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        url: String,
+        deleteTimeout: { type: Number, default: 500 },
+        confirmText: { type: String, default: "Are you sure?" },
+        successText: { type: String, default: "Deleted" },
+        errorText: { type: String, default: "Something went wrong!" },
+    },
+
+    data() {
+        return {
+            status: 'idle',
+        }
+    }, 
+
+    methods: {
+        click() {
+            switch (this.status) {
+                case 'idle':
+                case 'error':
+                    this.status = 'confirm';
+                    this.timeout('confirm');
+                    break;
+                case 'confirm':
+                    this.status = 'loading';
+                    this.submit();
+                    break;
+            }
+        },
+
+        submit() {
+            Promise.all([
+                // Real promise.
+                this.url ? axios.delete(this.url).catch(_ => new Error) : null,
+
+                // Promise ensuring at least 300ms of execution. 
+                new Promise((resolve, _) => setTimeout(resolve, 300))
+            ])
+
+            // Succeeds or fails based on axios's return value only.
+            .then(([value]) => value instanceof Error ? this.onError() : this.onSuccess());
+        },
+
+        onSuccess() {
+            this.status = 'success';
+            setTimeout(() => {
+                this.$emit('delete');
+            }, this.deleteTimeout);
+        },
+
+        onError() {
+            this.status = 'error';
+            this.timeout('error');
+        },
+
+        timeout(initialStatus) {
+            setTimeout(() => {
+                if (this.status === initialStatus) {
+                    this.status = 'idle';
+                }
+            }, 3000);
+        }
+    },
+
+    computed: {
+        showMessage() {
+            return ['confirm', 'success', 'error'].includes(this.status);
+        },
+
+        message() {
+            return this[this.status + 'Text'] || '';
+        },
+
+        messageClass() {
+            return this.status === 'confirm' ? 'text-muted' : 'text-danger';
+        }
+    }
+}
+</script>
+
+<style>
+    /* Fontawesome. */
+    .fa { cursor: pointer; color: #aaa; }
+    .fa:hover { color: #555; }
+    .fa.red { color: #CC0044; }
+    .fa.red:hover { color: #AA0022; }
+
+    /* Fade transition. */
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
---
+++ input-file/InputFile.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="input-group">
+        <input 
+            ref="hidden-input"
+            class="d-none" 
+            type="file" 
+            :name="name" 
+            :multiple="isMultiple"
+            autocomplete="off" 
+            @change="inputFileChanged" 
+        >
+
+        <input 
+            class="form-control"
+            type="text" 
+            v-model="fileNames" 
+            readonly
+            @click="triggerInputClick" 
+        >
+
+        <span class="input-group-btn">
+            <button 
+                class="btn btn-outline-secondary" 
+                type="button" 
+                @click="triggerInputClick"
+            >
+                <slot>Select...</slot>
+            </button>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'multiple'],
+
+    data() {
+        return {
+            fileNames: ''
+        }
+    },
+
+    computed: {
+        isMultiple() {
+            return typeof this.multiple !== 'undefined';
+        }
+    },
+
+    methods: {
+        triggerInputClick(event) {
+            this.$refs['hidden-input'].click();
+        },
+
+        inputFileChanged(event) {
+            this.$emit('change', event.target.files);
+            this.updateText(event);
+        },
+
+        updateText(event) {
+            let fileNames = '';
+            let selectedFiles = event.target.files;
+
+
+            for (var i = 0; i < selectedFiles.length; i++) {
+                fileNames += selectedFiles[i].name + ', ';
+            }
+
+            fileNames = fileNames.replace(/,\s*$/, '');
+
+            this.fileNames = fileNames;
+        }
+    }
+}
+</script>
+
+<style>
+    .form-control, .btn-outline-secondary {
+        border-color: #868e96 !important;
+    }
+</style>
